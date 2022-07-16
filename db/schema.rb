bf38114acff3a2ba970237dbb839073f76fffc55
Vinh <vinhbuith32412000@gmail.com>
2022-07-16 20:00:21 +0700

    Create Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_16_125329) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_16_125612) do
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
