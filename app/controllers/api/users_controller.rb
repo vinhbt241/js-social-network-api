65ed179f722af7b4a9ee78944723ea1dd19c76d3
Vinh <vinhbuith32412000@gmail.com>
2022-07-17 09:38:46 +0700

    Add route and controller to get user's friends

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,4 +4,10 @@ class Api::UsersController < ApplicationController
 
     render json: @user
   end
+
+  def friends 
+    @user = User.find(params[:user_id])
+
+    render json: @user.friends
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
     resources :users, only: [:show]
     resources :posts, only: [:show]
+
+    get '/users/:user_id/friends', to: 'users#friends'
   end
 end
