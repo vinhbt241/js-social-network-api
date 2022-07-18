450455c020270cc13f8af9bfa9414ab3470cd85f
Vinh <vinhbuith32412000@gmail.com>
2022-07-18 20:35:11 +0700

    Refactor seeds.rb for cleaner look

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
 main_user = User.create(name: "Vinh", is_online: true)
 
-main_post_one = Post.new(content: "What a beautiful day! Hope everyone is happy like me!")
-main_post_one.user = main_user
-main_post_one.save
+Post.create(content: "What a beautiful day! Hope everyone is happy like me!", user_id: 1)
+Post.create(content: "Gonna have a brunch today, anyone wanna join?", user_id: 1)
 
-main_post_two = Post.new(content: "Gonna have a brunch today, anyone wanna join?")
-main_post_two.user = main_user
-main_post_two.save
-
+#Create friends
 friend_one = User.create(name: "Viktor", is_online: true)
 friend_two = User.create(name: "John", is_online: false)
 friend_three = User.create(name: "Julia", is_online: false)
@@ -18,27 +14,18 @@ friend_one.friends.push(main_user)
 friend_two.friends.push(main_user)
 friend_three.friends.push(main_user)
 
-# Test friend posts
-friend_one_post_one = Post.new(content: "Hello, I'm Viktor, Vinh definitely can see this post!")
-friend_one_post_one.user = friend_one
-friend_one_post_one.save
+#Test friend posts
+Post.create(content: "Hello, I'm Viktor, Vinh definitely can see this post!", user_id: 2)
+Post.create(content: "Second post of Viktor haha!", user_id: 2)
+Post.create(content: "Hello, I'm John, Vinh definitely can see this post!", user_id: 3)
+Post.create(content: "Hello, I'm Julia, Vinh definitely can see this post!", user_id: 4)
 
-friend_one_post_two = Post.new(content: "Second post of Viktor haha!")
-friend_one_post_two.user = friend_one
-friend_one_post_two.save
-
-friend_two_post_one = Post.new(content: "Hello, I'm John, Vinh definitely can see this post!")
-friend_two_post_one.user = friend_two
-friend_two_post_one.save
-
-friend_three_post_one = Post.new(content: "Hello, I'm Julia, Vinh definitely can see this post!")
-friend_three_post_one.user = friend_three
-friend_three_post_one.save
-
+#Test comments
 Comment.create(content: "That is WHOLESOME, thanks Vinh!", post_id: 1, user_id: 2)
 Comment.create(content: "You have a nice day too, Vinh <3", post_id: 1, user_id: 4)
 Comment.create(content: "Almost forgot, remember to stop by my house today for your gift card!", post_id: 1, user_id: 4)
 
+#Test likes
 Like.create(post_id: 1, user_id: 1)
 Like.create(post_id: 1, user_id: 2)
 Like.create(post_id: 1, user_id: 3)
