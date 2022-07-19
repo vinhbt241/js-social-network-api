a09c8e1eaf2a1d7ae84083262953439cae9b5df5
Vinh <vinhbuith32412000@gmail.com>
2022-07-19 09:17:10 +0700

    Change order which like and comment where sorted

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,14 +1,14 @@
 class Api::PostsController < ApplicationController
   def comments 
     @post = Post.find(params[:id])
-    @comments = @post.comments.order(:created_at)
+    @comments = @post.comments.order(created_at: :desc)
 
     render json: @comments.to_json(include: [:user])
   end
 
   def likes 
     @post =  Post.find(params[:id])
-    @likes = @post.likes.order(:created_at)
+    @likes = @post.likes.order(created_at: :desc)
 
     render json: @likes.to_json(include: [:user])
   end
