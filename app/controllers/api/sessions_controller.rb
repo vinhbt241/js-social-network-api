f5ecfaf5625aa4073ffbabffcb5bd90aa15f3368
Vinh <vinhbuith32412000@gmail.com>
2022-07-26 16:03:11 +0700

    send user's info to frontend when session created

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::SessionsController < ApplicationController
 
     if user&.valid_password?(params[:password]) 
       token = jwt_encode(user_id: user.id)
-      render json: { token: token }, status: :ok
+      render json: { token: token, user: UserSerializer.new(user).serializable_hash[:data][:attributes] }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
