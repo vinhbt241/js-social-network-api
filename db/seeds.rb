3423d93b1cd0e09f477bd9d09c75fed47ccad1ad
Vinh <vinhbuith32412000@gmail.com>
2022-07-31 21:23:34 +0700

    Change path with image was read in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,7 @@ for i in 1..15
   )
 
   current_post.image.attach(
-    io: File.open("./post_images/post_#{i}.jpg"),
+    io: File.open(File.join(File.dirname(__FILE__), "/post_images/post_#{i}.jpg")),
     filename: "post_#{i}.jpg",
     content_type: 'image/jpg'
   )
